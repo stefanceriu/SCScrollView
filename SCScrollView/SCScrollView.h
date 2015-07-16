bbd8d939aa544ff766f1590256573da53d8d0dbe
Stefan Ceriu <stefan.ceriu@yahoo.com>
2015-07-16 20:27:49 +0100

    Replaced the touch refusal area with touch approval areas and added a directions

--- SCScrollView/SCScrollView.h
+++ SCScrollView/SCScrollView.h
@@ -9,20 +9,35 @@
 #import <UIKit/UIKit.h>
 
 @protocol SCEasingFunctionProtocol;
+@class SCScrollViewTouchApprovalArea;
 
 @interface SCScrollView : UIScrollView
 
-@property (nonatomic, strong) UIBezierPath *touchRefusalArea;
-
 @property (nonatomic, assign) NSUInteger maximumNumberOfTouches;
 
+@property (nonatomic, strong, readonly) NSArray *touchApprovalAreas;
+
 - (void)setContentOffset:(CGPoint)contentOffset
-          easingFunction:(id<SCEasingFunctionProtocol>)easingFunction
-                duration:(CFTimeInterval)duration
-              completion:(void(^)())completion;
+		  easingFunction:(id<SCEasingFunctionProtocol>)easingFunction
+				duration:(CFTimeInterval)duration
+			  completion:(void(^)())completion;
+
+
+- (void)addTouchApprovalArea:(SCScrollViewTouchApprovalArea *)touchApprovalArea;
+
+- (void)removeTouchApprovalArea:(SCScrollViewTouchApprovalArea *)touchApprovalArea;
+
 
 - (BOOL)isRunningAnimation;
 
 - (void)stopRunningAnimation;
+
+@end
+
+@interface SCScrollViewTouchApprovalArea : NSObject
+
+@property (nonatomic, strong) UIBezierPath *path;
+
+@property (nonatomic, assign) UISwipeGestureRecognizerDirection direction;
 
 @end
--- SCScrollView/SCScrollView.m
+++ SCScrollView/SCScrollView.m
@@ -12,10 +12,15 @@
 #import "SCWeakObjectWrapper.h"
 
 @interface UIView (SCFindFirstResponder)
+
 - (id)scFindFirstResponder;
+
 @end
 
+
 @interface SCScrollView ()
+
+@property (nonatomic, strong) NSMutableArray *approvalAreas;
 
 @property (nonatomic, strong) CADisplayLink *displayLink;
 
@@ -35,33 +40,69 @@
 
 - (void)dealloc
 {
-    [self.displayLink invalidate];
+	[self.displayLink invalidate];
 }
 
 - (id)initWithFrame:(CGRect)frame
 {
-    if(self = [super initWithFrame:frame]) {
-        self.maximumNumberOfTouches = NSUIntegerMax;
-    }
-    
-    return self;
+	if(self = [super initWithFrame:frame]) {
+		_maximumNumberOfTouches = NSUIntegerMax;
+		_approvalAreas = [NSMutableArray array];
+	}
+	
+	return self;
 }
+
+- (NSArray *)touchApprovalAreas
+{
+	return [self.approvalAreas copy];
+}
+
+- (void)addTouchApprovalArea:(SCScrollViewTouchApprovalArea *)touchApprovalArea
+{
+	if(!touchApprovalArea) {
+		return;
+	}
+	
+	[self.approvalAreas addObject:touchApprovalArea];
+}
+
+- (void)removeTouchApprovalArea:(SCScrollViewTouchApprovalArea *)touchApprovalArea
+{
+	[self.approvalAreas removeObject:touchApprovalArea];
+}
+
 
 #pragma mark - UIGestureRecognizerDelegate
 
 - (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer
 {
-    if([gestureRecognizer isKindOfClass:[UIPanGestureRecognizer class]]) {
-        
-        if(gestureRecognizer.numberOfTouches > self.maximumNumberOfTouches) {
-            return NO;
-        }
-        
-        CGPoint touchPoint = [gestureRecognizer locationInView:self];
-        return ![self.touchRefusalArea containsPoint:touchPoint];
-    }
-    
-    return YES;
+	if([gestureRecognizer isKindOfClass:[UIPanGestureRecognizer class]]) {
+		
+		if(gestureRecognizer.numberOfTouches > self.maximumNumberOfTouches) {
+			return NO;
+		}
+		
+		CGPoint touchPoint = [gestureRecognizer locationInView:self];
+		CGPoint velocity = [(UIPanGestureRecognizer *)gestureRecognizer velocityInView:self];
+		
+		BOOL shouldBegin = (self.touchApprovalAreas.count == 0);
+		for(SCScrollViewTouchApprovalArea *touchApprovalArea in self.approvalAreas) {
+			if([touchApprovalArea.path containsPoint:touchPoint]) {
+				
+				if((velocity.y < 0.0f && (touchApprovalArea.direction & UISwipeGestureRecognizerDirectionUp))    ||
+				   (velocity.x < 0.0f && (touchApprovalArea.direction & UISwipeGestureRecognizerDirectionLeft))  ||
+				   (velocity.x > 0.0f && (touchApprovalArea.direction & UISwipeGestureRecognizerDirectionRight))  ||
+				   (velocity.y > 0.0f && (touchApprovalArea.direction & UISwipeGestureRecognizerDirectionDown))) {
+					shouldBegin = YES;
+				}
+			}
+		}
+		
+		return shouldBegin;
+	}
+	
+	return YES;
 }
 
 #pragma mark - Public
@@ -72,40 +113,40 @@
 }
 
 - (void)setContentOffset:(CGPoint)contentOffset
-          easingFunction:(id<SCEasingFunctionProtocol>)easingFunction
-                duration:(CFTimeInterval)duration
-              completion:(void(^)())completion
+		  easingFunction:(id<SCEasingFunctionProtocol>)easingFunction
+				duration:(CFTimeInterval)duration
+			  completion:(void(^)())completion
 {
 	if(self.animationCompletionBlock) {
 		self.animationCompletionBlock();
 		self.animationCompletionBlock = nil;
 	}
 	
-    if(CGPointEqualToPoint(self.contentOffset, contentOffset) || duration == 0.0f) {
-        
-        self.contentOffset = contentOffset;
-        
-        if(completion) {
-            completion();
-        }
-        return;
-    }
-    
-    if (self.displayLink == nil) {
-        SCWeakObjectWrapper *target = [[SCWeakObjectWrapper alloc] initWithObject:self];
-        self.displayLink = [CADisplayLink displayLinkWithTarget:target selector:@selector(_updateContentOffset:)];
-        [self.displayLink setFrameInterval:1];
-        [self.displayLink setPaused:YES];
-        [self.displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
-    }
-    
-    self.duration = duration;
-    self.easingFunction = easingFunction;
-    self.animationCompletionBlock = completion;
-    self.deltaContentOffset = CGPointMake(contentOffset.x - self.contentOffset.x, contentOffset.y - self.contentOffset.y);
-    self.startTime = 0.0f;
-    
-    [self.displayLink setPaused:NO];
+	if(CGPointEqualToPoint(self.contentOffset, contentOffset) || duration == 0.0f) {
+		
+		self.contentOffset = contentOffset;
+		
+		if(completion) {
+			completion();
+		}
+		return;
+	}
+	
+	if (self.displayLink == nil) {
+		SCWeakObjectWrapper *target = [[SCWeakObjectWrapper alloc] initWithObject:self];
+		self.displayLink = [CADisplayLink displayLinkWithTarget:target selector:@selector(_updateContentOffset:)];
+		[self.displayLink setFrameInterval:1];
+		[self.displayLink setPaused:YES];
+		[self.displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
+	}
+	
+	self.duration = duration;
+	self.easingFunction = easingFunction;
+	self.animationCompletionBlock = completion;
+	self.deltaContentOffset = CGPointMake(contentOffset.x - self.contentOffset.x, contentOffset.y - self.contentOffset.y);
+	self.startTime = 0.0f;
+	
+	[self.displayLink setPaused:NO];
 }
 
 - (void)stopRunningAnimation
@@ -130,64 +171,69 @@
 
 - (void)_updateContentOffset:(CADisplayLink *)displayLink
 {
-    if(self.startTime == 0.0f) {
-        self.startTime = self.displayLink.timestamp;
-        self.startContentOffset = self.contentOffset;
-        return;
-    }
-    
-    CGFloat ratio = (CGFloat) ((displayLink.timestamp - self.startTime) / self.duration);
-    ratio = (1.0f - ratio < 0.01f) ? 1.0f : [self.easingFunction solveForInput:ratio];
-    
-    self.contentOffset = CGPointMake(self.startContentOffset.x + (self.deltaContentOffset.x * ratio),
-                                     self.startContentOffset.y + (self.deltaContentOffset.y * ratio));
-    
-    if (ratio == 1.0f) {
-        [self.displayLink setPaused:YES];
-        
-        if([self.delegate respondsToSelector:@selector(scrollViewDidEndScrollingAnimation:)]) {
-            [self.delegate scrollViewDidEndScrollingAnimation:self];
-        }
-        
-        if(self.animationCompletionBlock) {
-            self.animationCompletionBlock();
-            self.animationCompletionBlock = nil;
-        }
-    }
+	if(self.startTime == 0.0f) {
+		self.startTime = self.displayLink.timestamp;
+		self.startContentOffset = self.contentOffset;
+		return;
+	}
+	
+	CGFloat ratio = (CGFloat) ((displayLink.timestamp - self.startTime) / self.duration);
+	ratio = (1.0f - ratio < 0.01f) ? 1.0f : [self.easingFunction solveForInput:ratio];
+	
+	self.contentOffset = CGPointMake(self.startContentOffset.x + (self.deltaContentOffset.x * ratio),
+									 self.startContentOffset.y + (self.deltaContentOffset.y * ratio));
+	
+	if (ratio == 1.0f) {
+		[self.displayLink setPaused:YES];
+		
+		if([self.delegate respondsToSelector:@selector(scrollViewDidEndScrollingAnimation:)]) {
+			[self.delegate scrollViewDidEndScrollingAnimation:self];
+		}
+		
+		if(self.animationCompletionBlock) {
+			self.animationCompletionBlock();
+			self.animationCompletionBlock = nil;
+		}
+	}
 }
 
 - (void)scrollRectToVisible:(CGRect)rect animated:(BOOL)animated
 {
-    //Ignoring calls from any textField contained by this scrollview
-    id firstResponder = [self scFindFirstResponder];
+	id firstResponder = [self scFindFirstResponder];
 	if([firstResponder isKindOfClass:[UITextField class]] ||
 	   [firstResponder isKindOfClass:[UISearchBar class]] ||
 	   [firstResponder isKindOfClass:[UITextView class]]) {
 		return;
 	}
 	
-    [super scrollRectToVisible:rect animated:animated];
+	[super scrollRectToVisible:rect animated:animated];
 }
 
 @end
+
+
+@implementation SCScrollViewTouchApprovalArea
+
+@end
+
 
 @implementation UIView (SCFindFirstResponder)
 
 - (id)scFindFirstResponder
 {
-    if (self.isFirstResponder) {
-        return self;
-    }
-    
-    for (UIView *subview in self.subviews) {
-        id responder = [subview scFindFirstResponder];
-        
-        if (responder) {
-            return responder;
-        }
-    }
-    
-    return nil;
+	if (self.isFirstResponder) {
+		return self;
+	}
+	
+	for (UIView *subview in self.subviews) {
+		id responder = [subview scFindFirstResponder];
+		
+		if (responder) {
+			return responder;
+		}
+	}
+	
+	return nil;
 }
 
 @end
